838496a14422c3eb0ae8f66a1307553aea48f82c
Anton Verinov <git@zemlan.in>
2015-01-09 17:29:24 +0200

    client.js in index.html; remove console.log() in client.js

--- src/js/client.js
+++ src/js/client.js
@@ -4,13 +4,11 @@ var React = require('react/addons');
 var firebacon = require('./firebacon');
 var components = require('./components');
 
-var valueStream = firebacon.childOnValue('tests/f');
 var clickStream = new Bacon.Bus();
 
 clickStream.onValue(function (value) {
   firebacon.setChildValue('tests/f', value.newState.buttonText);
 });
-valueStream.onValue(function (snapshot) { console.log(snapshot.val()) });
 
 var MagicButton = components.MagicButton;
 
