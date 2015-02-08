43e1eacd2039ffaecca5884ed0747f84578a3a51
Anton Verinov <git@zemlan.in>
2015-02-08 14:18:24 +0200

    ƒ.fromKey curried and uncurried

--- src/js/funcy.js
+++ src/js/funcy.js
@@ -1,12 +1,18 @@
 "use strict";
 
+function _createObj(k, v) {
+  var result = {};
+  result[k] = v;
+  return result;
+}
+
 module.exports = {
   ply: function (f) { return Function.apply.bind(f, null); },
-  fromKey: function (key) {
-    return function (value) {
-      var result = {};
-      result[key] = value;
-      return result;
-    };
+  fromKey: function (key, value) {
+    if (arguments.length === 1) {
+      return _createObj.bind(null, key);
+    }
+
+    return _createObj(key, value);
   }
 };
