fb22abd910e05709f3c362b0ac62f14ef1e2343b
Anton Verinov <git@zemlan.in>
2015-05-01 20:13:28 +0300

    grid; debug cell

--- src/js/castInit.js
+++ src/js/castInit.js
@@ -12,20 +12,30 @@ function streamForReceiverManager(castReceiverManager) {
   )
 }
 
-function streamForMessageBus(messageBus) {
-  return Observable.fromEvent(messageBus, 'message')
-}
-
 if (config.cast.enabled) {
   cast.receiver.logger.setLevelValue(cast.receiver.LoggerLevel[config.cast.logLevel])
 
   window.castReceiverManager = cast.receiver.CastReceiverManager.getInstance()
   window.receiverManagerStream = streamForReceiverManager(window.castReceiverManager)
   window.messageBus = window.castReceiverManager.getCastMessageBus('urn:x-cast:blackwidow')
-  window.messageBusStream = streamForMessageBus(window.messageBus)
+  window.messageBusStream = Observable.fromEvent(window.messageBus, 'message')
 
   var appConfig = new cast.receiver.CastReceiverManager.Config()
   appConfig.statusText = 'Ready to play'
   appConfig.maxInactivity = 6000
   window.castReceiverManager.start(appConfig)
+} else {
+  var pingButton = document.createElement('button')
+  pingButton.textContent = 'ping'
+
+  window.messageBusStream = Observable.fromEvent(pingButton, 'click').map({"data": "ping"})
+  window.receiverManagerStream = Observable.create(obs => {
+    obs.onNext({"type": "ready"})
+    obs.onCompleted()
+  })
+
+  document.addEventListener("DOMContentLoaded", () => {
+    var widgets = document.getElementsByClassName('widget')
+    widgets[widgets.length - 1].appendChild(pingButton)
+  })
 }
