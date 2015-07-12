dcde61b29fbaa5c9563941f1217ab6480b7b815b
Anton Verinov <git@zemlan.in>
2015-07-12 11:10:19 +0300

    mockDashes[0] is now correct json

--- src/js/storage_adapters/mockDashes.js
+++ src/js/storage_adapters/mockDashes.js
@@ -1,183 +1,242 @@
-'use strict'
-
-import {hawkeyeEndpoint} from 'config'
+"use strict"
 
 export var mockDashes = {
   0: {
-    widgets: {
-      "2020a34a-87d2-45f9-8157-524ec2ddb143": {
-        type: 'text',
-        container: {
-          position: [0, 0], size: [4, 2], background: 'green',
+    "widgets": {
+      "duty": {
+        "type": "text",
+        "container": {
+          "position": [0, 0],
+          "size": [4, 2],
+          "background": "green",
         },
-        endpoint: hawkeyeEndpoint+'/duty',
-        endpointMethod: 'POST',
-        endpointSchedule: {
-          type: ['timeInterval'],
-          timeInterval: 60 * 60 * 1000,
-        },
-        data: {
-          note: '/duty',
-        }
-      },
-      "30e46823-3175-43a6-9a89-203eb763ae63": {
-        type: 'text',
-        container: {
-          position: [4, 0], size: [4, 2], background: 'orange',
-        },
-        endpoint: hawkeyeEndpoint+'/text?type=default',
-        endpointMethod: 'POST',
-        endpointSchedule: {
-          type: ['timeInterval'],
-          timeInterval: 10 * 60 * 1000,
-        },
-        data: {
-          note: '/text default',
-        }
-      },
-      "30e46823-3175-43a6-9a89-203eb763ae67": {
-        type: 'image',
-        container: {
-          position: [8, 0], size: [2, 2], background: 'orange',
-          fontSize: '5em',
-        },
-        endpoint: 'http://api.openweathermap.org/data/2.5/weather?q=Kiev&units=metric',
-        endpointSchedule: {
-          type: ['timeInterval'],
-          timeInterval: 10 * 60 * 1000,
-        },
-        endpointMap: {
-          "src": {
-            _format: "http://openweathermap.org/img/w/{}.png",
-            _path: "weather.0.icon",
+        "endpoint": {
+          "url": "https://blackwidow.parseapp.com/duty",
+          "method": "POST",
+          "schedule": {
+            "type": ["timeInterval"],
+            "timeInterval": 3600,
           },
-          "note": {
-            _format: "{}°C",
-            _path: "main.temp",
-          }
+        },
+        "data": {
+          "note": "/duty",
         }
       },
-      "30e46823-3175-43a6-9a89-203eb763fe63": {
-        type: 'text',
-        container: {
-          position: [0, 2], size: [6, 3], background: 'orange',
+      "defaultVersion": {
+        "type": "text",
+        "container": {
+          "position": [4, 0],
+          "size": [4, 2],
+          "background": "orange",
         },
-        endpoint: hawkeyeEndpoint+'/text?type=quote',
-        endpointMethod: 'POST',
-        endpointSchedule: {
-          type: ['timeInterval'],
-          timeInterval: 20 * 60 * 1000,
+        "endpoint": {
+          "url": "https://blackwidow.parseapp.com/text?type=default",
+          "method": "POST",
+          "schedule": {
+            "type": ["timeInterval"],
+            "timeInterval": 600,
+          },
         },
-        data: {
-          note: '/text quote',
+        "data": {
+          "note": "/text default",
         }
       },
-      "a49953a1-2c7e-4ae6-b1b8-2d6767c0fd47": {
-        type: 'image',
-        container: {
-          position: [6, 2], size: [4, 8], background: 'maroon',
+      "weather": {
+        "type": "image",
+        "container": {
+          "position": [8, 0],
+          "size": [2, 2],
+          "background": "orange",
+          "fontSize": "5em",
         },
-        endpoint: hawkeyeEndpoint+"/text?type=gif",
-        endpointMap: {"src": "text"},
-        endpointMethod: 'POST',
-        endpointSchedule: {
-          type: ['timeInterval'],
-          timeInterval: 15 * 60 * 1000,
+        "endpoint": {
+          "url": "http://api.openweathermap.org/data/2.5/weather?q=Kiev&units=metric",
+          "schedule": {
+            "type": ["timeInterval"],
+            "timeInterval": 600,
+          },
+          "map": {
+            "src": {
+              _format: "http://openweathermap.org/img/w/{}.png",
+              _path: "weather.0.icon",
+            },
+            "note": {
+              _format: "{}°C",
+              _path: "main.temp",
+            },
+          },
         },
-        data: {
-          note: '/text gif',
+      },
+      "quote": {
+        "type": "text",
+        "container": {
+          "position": [0, 2],
+          "size": [6, 3],
+          "background": "orange",
+        },
+        "endpoint": {
+          "url": "https://blackwidow.parseapp.com/text?type=quote",
+          "method": "POST",
+          "schedule": {
+            "type": ["timeInterval"],
+            "timeInterval": 1200,
+          },
+        },
+        "data": {
+          "note": "/text quote",
         }
       },
-      "c8ff8eef-aae4-4acc-8152-5a98fbd00ddd": {
-        type: 'text',
-        container: {
-          position: [3, 5], size: [3, 5], background: 'maroon',
-          fontSize: '10em'
+      "gif": {
+        "type": "image",
+        "container": {
+          "position": [6, 2],
+          "size": [4, 8],
+          "background": "maroon",
         },
-        endpoint: hawkeyeEndpoint+"/text?type=uno",
-        endpointMethod: 'POST',
-        endpointSchedule: {
-          type: ['timeInterval'],
-          timeInterval: 30 * 60 * 1000,
+        "endpoint": {
+          "url": "https://blackwidow.parseapp.com/text?type=gif",
+          "method": "POST",
+          "schedule": {
+            "type": ["timeInterval"],
+            "timeInterval": 900,
+          },
+          "map": {
+            "src": "text",
+          },
         },
-        data: {
-          note: '/text uno',
+        "data": {
+          "note": "/text gif",
         }
       },
-      "30e46823-3175-43a6-9a89-203eb763fe65": {
-        type: 'text',
-        container: {
-          position: [0, 5], size: [3, 1], background: 'orange',
-          fontSize: '5em',
+      "uno": {
+        "type": "text",
+        "container": {
+          "position": [3, 5],
+          "size": [3, 5],
+          "background": "maroon",
+          "fontSize": "10em",
         },
-        endpoint: 'https://ai.uaprom/stats/uaprom',
-        endpointSchedule: {
-          type: ['timeInterval'],
-          timeInterval: 1 * 60 * 1000,
+        "endpoint": {
+          "url": "https://blackwidow.parseapp.com/text?type=uno",
+          "method": "POST",
+          "schedule": {
+            "type": ["timeInterval"],
+            "timeInterval": 1800,
+          },
         },
-        endpointPath: 'last_migration',
-        endpointMap: {"text": "target_title"},
-        data: { note: 'UA', }
+        "data": {
+          "note": "/text uno",
+        }
       },
-      "30e46823-3175-43a6-9a89-203eb763fe66": {
-        type: 'text',
-        container: {
-          position: [0, 6], size: [3, 1], background: 'orange',
-          fontSize: '5em',
+      "productionUA": {
+        "type": "text",
+        "container": {
+          "position": [0, 5],
+          "size": [3, 1],
+          "background": "orange",
+          "fontSize": "5em",
         },
-        endpoint: 'https://ai.uaprom/stats/ruprom',
-        endpointSchedule: {
-          type: ['timeInterval'],
-          timeInterval: 1 * 60 * 1000,
+        "endpoint": {
+          "url": "https://ai.uaprom/stats/uaprom",
+          "schedule": {
+            "type": ["timeInterval"],
+            "timeInterval": 60,
+          },
+          "map": {
+            "text": "last_migration.target_title",
+          },
         },
-        endpointPath: 'last_migration',
-        endpointMap: {"text": "target_title"},
-        data: { note: 'RU', }
+        "data": {
+          "note": "UA",
+        }
       },
-      "30e46823-3175-43a6-9a89-203eb763fe67": {
-        type: 'text',
-        container: {
-          position: [0, 7], size: [3, 1], background: 'orange',
-          fontSize: '5em',
+      "productionRU": {
+        "type": "text",
+        "container": {
+          "position": [0, 6],
+          "size": [3, 1],
+          "background": "orange",
+          "fontSize": "5em",
         },
-        endpoint: 'https://ai.uaprom/stats/byprom',
-        endpointSchedule: {
-          type: ['timeInterval'],
-          timeInterval: 1 * 60 * 1000,
+        "endpoint": {
+          "url": "https://ai.uaprom/stats/ruprom",
+          "schedule": {
+            "type": ["timeInterval"],
+            "timeInterval": 60,
+          },
+          "map": {
+            "text": "last_migration.target_title",
+          },
         },
-        endpointPath: 'last_migration',
-        endpointMap: {"text": "target_title"},
-        data: { note: 'BY', }
+        "data": {
+          "note": "RU",
+        }
       },
-      "30e46823-3175-43a6-9a89-203eb763fe68": {
-        type: 'text',
-        container: {
-          position: [0, 8], size: [3, 1], background: 'orange',
-          fontSize: '5em',
+      "productionBY": {
+        "type": "text",
+        "container": {
+          "position": [0, 7],
+          "size": [3, 1],
+          "background": "orange",
+          "fontSize": "5em",
         },
-        endpoint: 'https://ai.uaprom/stats/kzprom',
-        endpointSchedule: {
-          type: ['timeInterval'],
-          timeInterval: 1 * 60 * 1000,
+        "endpoint": {
+          "url": "https://ai.uaprom/stats/byprom",
+          "schedule": {
+            "type": ["timeInterval"],
+            "timeInterval": 60,
+          },
+          "map": {
+            "text": "last_migration.target_title",
+          },
         },
-        endpointPath: 'last_migration',
-        endpointMap: {"text": "target_title"},
-        data: { note: 'KZ', }
+        "data": {
+          "note": "BY",
+        }
       },
-      "30e46823-3175-43a6-9a89-203eb763fe69": {
-        type: 'text',
-        container: {
-          position: [0, 9], size: [3, 1], background: 'orange',
-          fontSize: '5em',
+      "productionKZ": {
+        "type": "text",
+        "container": {
+          "position": [0, 8],
+          "size": [3, 1],
+          "background": "orange",
+          "fontSize": "5em",
         },
-        endpoint: 'https://ai.uaprom/stats/mdprom',
-        endpointSchedule: {
-          type: ['timeInterval'],
-          timeInterval: 1 * 60 * 1000,
+        "endpoint": {
+          "url": "https://ai.uaprom/stats/kzprom",
+          "schedule": {
+            "type": ["timeInterval"],
+            "timeInterval": 60,
+          },
+          "map": {
+            "text": "last_migration.target_title",
+          },
         },
-        endpointPath: 'last_migration',
-        endpointMap: {"text": "target_title"},
-        data: { note: 'MD', }
+        "data": {
+          "note": "KZ",
+        }
+      },
+      "productionMD": {
+        "type": "text",
+        "container": {
+          "position": [0, 9],
+          "size": [3, 1],
+          "background": "orange",
+          "fontSize": "5em",
+        },
+        "endpoint": {
+          "url": "https://ai.uaprom/stats/mdprom",
+          "schedule": {
+            "type": ["timeInterval"],
+            "timeInterval": 60,
+          },
+          "map": {
+            "text": "last_migration.target_title",
+          },
+        },
+        "data": {
+          "note": "MD",
+        }
       },
     },
   }
