f619c538e3b868968342d0317cada0c541897b1d
Anton Verinov <git@zemlan.in>
2015-07-12 12:02:18 +0300

    load external dashes

--- src/js/store.js
+++ src/js/store.js
@@ -3,15 +3,50 @@
 import R from 'ramda'
 import _ from 'lodash'
 import {Subject, BehaviorSubject, Observable} from 'rx'
+import {DOM as RxDOM} from 'rx-dom'
 import mockDashes from './mockDashes'
 
-export function getDash(id) {
-  return Observable.return(mockDashes[id] || mockDashes[0])
+export function getDash() {
+  var id
+  var url
+
+  if (location.hash) {
+    if (location.hash.match(/^#id[0-9a-f]{1,10}$/i)) {
+      id = location.hash.replace(/^#id/, '')
+      return Observable.return(mockDashes[id] || mockDashes[0])
+    }
+
+    if (location.hash.match(/^#https?:/)) {
+      url = location.hash.replace(/^#/, '')
+      return RxDOM.ajax({
+        url,
+        responseType: 'text/plain',
+        contentType: 'application/json; charset=UTF-8',
+        crossDomain: true,
+      })
+      .map(data => JSON.parse(data.response))
+      .catch(err => Observable.return({
+        "widgets": {
+          "error": {
+            "type": "text",
+            "container": {
+              "position": [0, 0],
+              "size": [10, 10],
+              "background": "red",
+            },
+            "data": {"text": err.message},
+          },
+        },
+      }))
+    }
+  }
+
+  return Observable.return(mockDashes[0])
 }
 
 function StoreStream(name) {
   var pushStream = new Subject()
-  var dataStream = new BehaviorSubject()
+  var dataStream = new BehaviorSubject({})
 
   var dataSubscription = pushStream
     .filter(v => v && v.length === 2)
