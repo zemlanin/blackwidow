9dd4da66f89b6bbae3d49af17753e0e100a51aa6
Anton Verinov <git@zemlan.in>
2015-11-01 22:06:51 +0200

    value labels; rotate labels to fit into a bar width

--- src/js/components/widgets/graph.js
+++ src/js/components/widgets/graph.js
@@ -20,10 +20,19 @@ const COLORS = [
   '#00F',
 ]
 
-export default ({data, widgetId}) => {
-  data = data || {}
+function getTextWidth(text) {
+  return 33 * (text || '').toString().length // for font-size 50
+}
 
-  var {values, baseValues, sortBy} = data
+function getTextHeightShift(text, width) {
+  const valueLabelWidth = getTextWidth(text)
+  return width >= valueLabelWidth
+        ? 0
+        : valueLabelWidth * Math.sin(Math.acos(width / valueLabelWidth))
+}
+
+export default ({data, widgetId}) => {
+  var {values, baseValues, sortBy} = data || {}
   baseValues = baseValues || {}
   values = _.map(values, v => v.value ? v : {value: v})
 
@@ -45,15 +54,18 @@ export default ({data, widgetId}) => {
     max = _.max(values, 'value').value
   }
 
-  var baseDelta = max - min
-  const sizeCoef = (BOTTOM_BAR_LINE_Y - TOP_BAR_LINE_Y) / baseDelta
+  const sizeCoef = (BOTTOM_BAR_LINE_Y - TOP_BAR_LINE_Y) / (max - min)
 
   var lineY = BOTTOM_BAR_LINE_Y
   if (min < 0) {
     lineY = TOP_BAR_LINE_Y + max * sizeCoef
   }
 
-  var spaceBetweenBars = (RIGHT_BAR_LINE_X - LEFT_BAR_LINE_X - BAR_SPACING) / values.length
+  const spaceBetweenBars = (RIGHT_BAR_LINE_X - LEFT_BAR_LINE_X - BAR_SPACING) / values.length
+  const barWidth = spaceBetweenBars - BAR_SPACING
+
+  const valueLabelShift = _.max(_.map(values, v => getTextHeightShift(v.value, barWidth)))
+  const barLabelShift = _.max(_.map(values, v => getTextHeightShift(v.text, barWidth)))
 
   return React.createElement("svg", {
       xmlns: "http://www.w3.org/svg/2000",
@@ -71,11 +83,18 @@ export default ({data, widgetId}) => {
       strokeWidth: 6,
     }),
     _.map(values, (el, i) => {
-      const value = el.value
       const color = el.color || COLORS[i % COLORS.length]
+      const x = LEFT_BAR_LINE_X + BAR_SPACING + spaceBetweenBars * i
+      let y, height
 
-      var x = LEFT_BAR_LINE_X + BAR_SPACING + spaceBetweenBars * i
-      var y, height
+      let value
+      if (el.value < min) {
+        value = min
+      } else if (el.value > max) {
+        value = max
+      } else {
+        value = el.value
+      }
 
       if (value < 0) {
         y = lineY
@@ -87,24 +106,44 @@ export default ({data, widgetId}) => {
 
       return [
         React.createElement("rect", {
-          key: '0' + i,
+          key: 'bar' + i,
           fill: color,
           x: x,
           y: y,
-          width: spaceBetweenBars - BAR_SPACING,
+          width: barWidth,
           height: Math.max(height, 6),
           "data-value": value,
         }),
+
         React.createElement("path", {
-          key: '1' + i,
-          id: `path_bar_${widgetId}_${i}`,
+          key: 'valuelabelpath' + i,
+          id: `path_value_${widgetId}_${i}`,
           d: [
-            `M ${x + BAR_SPACING} ${BOTTOM_BAR_LINE_Y + 25 * 2}`,
-            `L ${800 + x + BAR_SPACING} ${300 + BOTTOM_BAR_LINE_Y + 25 * 2}`
+            `M ${x} ${y}`,
+            `L ${x + barWidth} ${y - valueLabelShift}`
           ].join(' ')
         }),
         React.createElement("text", {
-            key: '2' + i,
+            key: 'value_label' + i,
+            fill: color,
+            stroke: color,
+            fontSize: 50,
+          },
+          React.createElement("textPath", {
+            xlinkHref: `#path_value_${widgetId}_${i}`,
+          }, el.value)
+        ),
+
+        React.createElement("path", {
+          key: 'labelpath' + i,
+          id: `path_bar_${widgetId}_${i}`,
+          d: [
+            `M ${x} ${BOTTOM_BAR_LINE_Y + 50 + barLabelShift}`,
+            `L ${x + barWidth} ${BOTTOM_BAR_LINE_Y + 50}`
+          ].join(' ')
+        }),
+        React.createElement("text", {
+            key: 'label' + i,
             fill: color,
             stroke: color,
             fontSize: 50,
