6375187ccf46e144a2f456345c2e4ed20997ee57
Anton Verinov <git@zemlan.in>
2016-02-20 10:53:43 +0200

    ETags for github API calls

--- src/js/github.js
+++ src/js/github.js
@@ -3,6 +3,8 @@ import Rx from 'rx'
 import parse from 'url-parse'
 
 const BASE_API_URL = 'https://api.github.com'
+const resourceETags = {}
+
 export const api = (path) => {
   const token = localStorage.getItem('github:token')
   let headers = new Headers()
@@ -11,10 +13,22 @@ export const api = (path) => {
     headers.set('Authorization', `token ${token}`)
   }
 
+  if (resourceETags[path]) {
+    headers.set('If-None-Match', resourceETags[path])
+  }
+
   return Rx.Observable.fromPromise(
     fetch(BASE_API_URL + path, {
       headers,
-    }).then(_.method('json'))
+    })
+    .then((resp) => {
+      const ETag = resp.headers.get('ETag')
+      if (ETag) {
+        resourceETags[path] = ETag
+      }
+
+      return resp.json()
+    })
   )
 }
 
